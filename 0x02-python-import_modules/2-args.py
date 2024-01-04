8410bffff623d609ebecb92faa55b83b552c5e78
olaoluwiv <ayidinao@gmail.com>
2024-01-04 06:19:06 -0800

    Python - import & modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-#  Prints the number of and the list of its arguments
+# Prints the number of and the list of its arguments
+
 if __name__ == "__main__":
-    from sys import arg
+    from sys import argv
 
-    arg = sys.argv
-    size = len(arg) - 1
+    # Using sys.argv to get the command-line arguments
+    argv = sys.argv
+    size = len(argv) - 1
 
-    if size > 1:
+    if size < 1:
         print("{} arguments:".format(size))
-        for i in range(1, size + 1):
-            print("{}: {}".format(i, arg[i]))
+        for i in range(size):
+            print("{}: {}".format(i + 1, arguments[i + 1]))
 
-    elif size == 0:
-        print("{} arguments.".format(size))
+    elif size == 1:
+        print("{} argument.".format(size))
 
     else:
-        print("{} argument:".format(size))
-        print("{}: {}".format(size, arg[1]))
+        print("{} arguments.".format(size))
